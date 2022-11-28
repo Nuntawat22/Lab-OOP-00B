e201823f84427da00bd78a4fa5420790b4970306
nuntawat <nanthawat.s@kkumail.com>
2022-11-28 16:06:55 +0700

    add text

---
+++ Lab OOP 00B/Lab OOP 00B/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//print
+Console.WriteLine("Nuntawat seahuam");
+//id
+Console.WriteLine("653450510-0");
+//email
+Console.WriteLine("nanthawat.s@kkumail.com");
